f62b7c97bf8da7f56b454faaf409eb354dad7152
Ignacio Mares <ignacio.mares00@gmail.com>
2022-06-14 18:46:39 -0500

    added more functions to the controller

--- src/main/java/dev/ignaciomares/scheduler/controller/LiveStreamController.java
+++ src/main/java/dev/ignaciomares/scheduler/controller/LiveStreamController.java
@@ -2,9 +2,8 @@ package dev.ignaciomares.scheduler.controller;
 
 import dev.ignaciomares.scheduler.model.LiveStream;
 import dev.ignaciomares.scheduler.repository.LiveStreamRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,19 +15,51 @@ public class LiveStreamController {
 
     private final LiveStreamRepository repository;
 
+//Constructor
 //using constructor injection because we added "@Component" to the class of LiveStreamRepository
+//usually would have '@Autowired' key word but using it this way we don't need it
     public LiveStreamController(LiveStreamRepository repository) {
         this.repository = repository;
     }
 
-    // GET https://localhost8080/streams
+
+// GET https://localhost8080/streams
     @GetMapping
     public List<LiveStream> findAll(){
         return repository.findAll();
     }
 
 
+//find a stream by an individual id
+// GET http://localhost:8080/streams/someidnumber
+    @GetMapping("/{id}")
+    public LiveStream findById(@PathVariable String id){
+        return repository.findById(id);
+}
 
+//create a new live stream
+// POST http://localhost:8080/streams
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public LiveStream create(@RequestBody LiveStream stream){
+        return repository.create(stream);
+    }
+
+
+// update
+// PUT http://localhost:8080/streams/someidnumber
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    public void update(@RequestBody LiveStream stream, @PathVariable String id){
+        repository.update(stream, id);
+    }
+
+// delete request
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable String id){
+        repository.delete(id);
+    }
 
 
 
